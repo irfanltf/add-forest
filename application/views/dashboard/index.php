83755ffe2bf6731b681a8cab66f5e088d172af96
Muhammad Irfan Lutfi <iluthfi469@gmail.com>
2022-12-06 13:08:12 +0700

    dashboard kosong

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,18 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?php echo $title; ?></h1>
+
+
+    <div class="row">
+
+
+    </div>
+
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
